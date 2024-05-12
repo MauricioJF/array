7dcefc0ea670c7b883866dd4533eeed4a814bbfc
Mauiricio Miguel Garcia Martinica <maurymiguelgarcia@gmail.com>
2024-05-12 15:00:39 -0600

    array

---
+++ src/App.java
@@ -0,0 +1,75 @@
+java
+import java.util.Scanner;
+
+public class App {
+
+    public static int tamv = 0;
+    public static Scanner leer = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+
+        boolean condiciosalida = true;
+
+        do {
+
+            try {
+
+                System.out.println(
+                        "Ingrese el tamaño del arreglo que no supere los 16 y sea mayor a 1 y que tampoco sea negativo ");
+                tamv = leer.nextInt();//10
+                condiciosalida = (tamv >= 2 && tamv <= 16) ? false : true;
+
+            } catch (Exception e) {
+                System.out.println(e);
+                System.out.println("Ocurrio algo revisar");
+                leer.nextLine();
+            }
+
+        } while (condiciosalida);
+
+        // Recibimos el array del método
+        int array[] = llenado();
+
+        // Invertimos el array
+        int[] arrayInvertido = invertirArray(array);
+
+        System.out.println("Mostrar Array Invertido:");
+        Mostrar(arrayInvertido);
+
+    }
+
+    public static int[] llenado() {
+
+        int[] arreglo = new int[tamv];
+
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingrese el valor [" + i + "]");
+            arreglo[i] = leer.nextInt();
+        }
+
+        return arreglo;
+    }
+
+    public static void Mostrar(int[] a) {
+
+        for (int i : a) {
+            System.out.println(i);
+        }
+
+        // Puedes mejorar el formato de salida aquí si lo deseas
+
+    }
+
+    public static int[] invertirArray(int[] array) {
+        int[] arrayInvertido = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            arrayInvertido[i] = array[array.length - 1 - i];
+        }
+        return arrayInvertido;
+        int[] array = llenado();
+        int[] arrayInvertido = invertirArray(array);
+        System.out.println("Array Invertido:");
+        Mostrar(arrayInvertido);
+    }
+
+}
